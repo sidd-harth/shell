34bd1ef8e03705db31e1780d8aff4c35fbb641cd
Siddharth Barahalikar <barahalikar.siddharth@gmail.com>
2021-03-24 23:53:59 +0530

    apt-get update

--- k8s-setup.sh
+++ k8s-setup.sh
@@ -34,6 +34,7 @@ cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
 deb http://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 KUBE_VERSION=1.20.2
+apt-get update
 apt-get install -y docker.io kubelet=${KUBE_VERSION}-00 kubeadm=${KUBE_VERSION}-00 kubectl=${KUBE_VERSION}-00 kubernetes-cni=0.8.7-00
 
 cat > /etc/docker/daemon.json <<EOF
